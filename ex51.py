b45d870b1837666d6f349e4e2d3b0220c6f7c0ac
Ana Raquel Sena Leite <quelfort@users.noreply.github.com>
2021-01-12 12:02:34 -0300

    Create ex51.py

---
+++ ex51.py
@@ -0,0 +1,26 @@
+grade= input ("Qu'est que ce ton grade avec la lettre?")
+
+if grade == "A+":
+    print (" Ton grade est: 4.0")
+elif grade == "A":
+    print (" Ton grade est: 4.0")
+elif grade == "A-":
+    print (" Ton grade est: 3.7")
+elif grade == "B+":
+    print (" Ton grade est: 3.3")
+elif grade == "B-":
+    print (" Ton grade est: 2.7")
+elif grade == "C+":
+    print (" Ton grade est: 2.3")
+elif grade == "C":
+    print (" Ton grade est: 2.0")
+elif grade == "C-":
+    print (" Ton grade est: 1.7")
+elif grade == "D+":
+    print (" Ton grade est: 1.3")
+elif grade == "D":
+    print (" Ton grade est: 1.0")
+elif grade == "F":
+    print (" Ton grade est: 0")
+else:
+    print ("ERROR")
