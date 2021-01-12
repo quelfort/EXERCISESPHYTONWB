691bf5409bd529c5edc76b69cecd66b5d0b6b9e8
Ana Raquel Sena Leite <quelfort@users.noreply.github.com>
2021-01-12 12:02:06 -0300

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,33 @@
+'''pair or impair
+ If and else
+
+if  condiçãoo :
+    bloco verdadeiro
+else:
+    bloco falso
+'''
+#Usados para comparar dois valores
+
+# == Igual
+# != Diferente
+# > Maior que
+# < Menor que
+# >= Maior ou igual
+# <= Menor ou igual
+
+
+# + Adição
+# - Subtração
+# * Multiplicação
+# / Divisão
+# % resto
+# ** Exponenciação
+# // Resto da divisão
+
+n = int(input('ecrire un integer pour savoir si est il pair or impair:  '))
+
+
+if n%2==0:
+    print ("le numero", n ,"est pair")
+else:
+    print ("le numero", n ,"est inpair")
