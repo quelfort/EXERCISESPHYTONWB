f78a84f1c6338fc3fd5ed1d28a240aef10ae5fb7
Ana Raquel Sena Leite <quelfort@users.noreply.github.com>
2021-01-12 12:01:28 -0300

    Create ex15.py

---
+++ ex15.py
@@ -0,0 +1,5 @@
+df = float (input ("quelle est la distance en feet ?"))
+
+print ("La distance en in est:", df*12)
+print ("La distance en yards est:", df*0.33333)
+print ("La distance en miles est:", df*0.000189394)
