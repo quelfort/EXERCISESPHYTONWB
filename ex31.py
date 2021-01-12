a74fc799865289e744c13a6a5ecdab8ffd9efb4e
Ana Raquel Sena Leite <quelfort@users.noreply.github.com>
2021-01-12 12:01:57 -0300

    Create ex31.py

---
+++ ex31.py
@@ -0,0 +1,10 @@
+print ("Sum of the Digits in an Integer")
+
+n = input ("Write a 4 digits integer")
+
+n0= int (n[0])
+n1= int (n[1])
+n2= int (n[2])
+n3= int (n[3])
+
+print ("The sum of the digits from ", n, "it's:",n[0], "+", n[1], "+", n[2], "+",n[3], "=", n0+n1+n2+n3)
